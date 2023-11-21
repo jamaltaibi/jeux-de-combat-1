2dbe94dd517be0edab9bb99e2de9d53152c61a0a
Your Name <jamaltaibi@MacBook-Pro-de-Jamal.local>
2023-11-21 11:49:17 +0100

    jeuxdecombat1

---
+++ script/script.js
@@ -0,0 +1,32 @@
+const J1 = document.querySelector(`.joueur1`)
+const J2 = document.querySelector(`joueur2`)
+
+const select = document.querySelector(`.personnage`)
+
+let persoG = {
+    "vie" : "100%",
+    "point d'attaque" : "25%",
+    "point de défense" : "10%",
+    "capacite speciale" : "30%",
+}
+
+let persoM = {
+    "vie" : "100%",
+    "point d'attaque" : "20%",
+    "point de défense" : "15%",
+    "capacite speciale" : "40%",
+}
+
+let persoP ={
+    "vie" : "100%",
+    "point d'attaque" : "10%",
+    "point de défense" : "10%",
+    "capacite speciale" : "60%",
+}
+ let persoA = {
+    "vie" : "100%",
+    "point d'attaque" : "15%",
+    "point de défense" : "5%",
+    "capacite speciale" : "40%",
+}
+
