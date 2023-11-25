ddba14036648293df44d3c0a9b15ec971f40b0fe
Your Name <jamaltaibi@mbp-de-jamal.home>
2023-11-25 19:07:29 +0100

    jeudecombat

--- script/script.js
+++ script/script.js
@@ -12,6 +12,39 @@ const arene = document.getElementById(`Arene`);
 const zonedecombat = document.getElementById('zonedecombat')
 const manette1 = document.querySelectorAll('.manette button')
 const manette2 = document.querySelectorAll('.manette2 button')
+const vie = document.querySelectorAll(".vie, .vie2")
+
+let persoG = {
+    "vie" : "100",
+    "pointdattaque" : "30",
+    "pointdedéfense" : "10",
+    "capacitespeciale" : "30",
+}
+let persoG2 = {
+    "vie" : "100",
+    "pointdattaque" : "30",
+    "pointdedéfense" : "10",
+    "capacitespeciale" : "30",
+}
+
+let persoM = {
+    "vie" : "100%",
+    "pointdattaque" : "20%",
+    "pointdedéfense" : "10%",
+    "capacitespeciale" : "40%",
+}
+let persoP ={
+    "vie" : "100%",
+    "pointdattaque" : "15%",
+    "pointdedéfense" : "10%",
+    "capacitespeciale" : "60%",
+}
+ let persoA = {
+    "vie" : "100%",
+    "point d'attaque" : "15%",
+    "point de défense" : "5%",
+    "capacite speciale" : "40%",
+}
 
 let joueur1Choisi = false;
 let joueur2Choisi = false;
@@ -127,61 +160,31 @@ newgame.addEventListener('click', () => {
     partie.style.display = 'none';
     arene.style.display = "none";
     zonedecombat.style.display = "flex";
-    // activerJoueur(joueurActif);
 })
 
-document.getElementById('attaquer').addEventListener('click', attaquer);
-document.getElementById('defense').addEventListener('click', defendre);
-document.getElementById('capacitespe').addEventListener('click', capaciteSpeciale);
-
-const pointsDeVieInitiaux = { joueur1: 100, joueur2: 100,};
-
-const pointsDeVieActuels = { joueur1: pointsDeVieInitiaux.joueur1, 
-    joueur2: pointsDeVieInitiaux.joueur2 };
-
-
-function mettreAJourPointsDeVie() {
-    document.querySelector('.vie1').style.width = `${pointsDeVieActuels.joueur1}%`;
-    document.querySelector('.vie2').style.width = `${pointsDeVieActuels.joueur2}%`;
-    if (pointsDeVieInitiaux = O ) {
-        "Partie fini "
-    }
-  // Ajoutez ici des conditions pour gérer la fin du jeu si les points de vie atteignent zéro
-}
-
-function attaquer() {
-    const degats = Math.floor(Math.random() * 20) + 10; 
-    // Exemple de dégâts aléatoires entre 10 et 30
-    // Appliquer les dégâts à l'adversaire
-    pointsDeVieActuels.joueur2 -= degats;
-    // Mettre à jour l'affichage des points de vie
-    mettreAJourPointsDeVie();
-  }
-
-function defendre() {
-    const reductionDegats = 0.5; // Exemple de réduction des dégâts de 50%
-    // Réduire les dégâts subis lors de la prochaine attaque
-    // Ajoutez ici la logique pour la défense
-  }
-
-function capaciteSpeciale() {
-    const degatsCapacite = 30; // Exemple de dégâts de capacité spéciale
-    // Appliquer les dégâts de capacité spéciale à l'adversaire
-    pointsDeVieActuels.joueur2 -= degatsCapacite;
-    // Mettre à jour l'affichage des points de vie
-    mettreAJourPointsDeVie();
-  }
-
+//perso Guerrier//
 for (const btn of manette1){
     btn.addEventListener('click', () => {
-        if (btn.id == "attaquer"){
-            attaquer
+        if (btn.id == "attaquer1"){
+
+            let degats = persoG2.pointdattaque - persoG2.pointdedéfense;
+            degats = Math.max(0, degats);
+            persoG2.vie -= degats;
+
+            if (persoG.vie <= 20) {
+                persoG.capacitespeciale;
+                }
+            persoG2.vie;
+            console.log(persoG.vie,"attaque 1");
         }
-        if (btn.id == "defense") {
-            defendre
+        if (btn.id == "defense1") {
+
+            (persoG.pointdedéfense + persoG.vie == persoG.pointdedéfense);
+           
+            console.log(persoG.pointdedéfense),"def1";
         }
-        if (btn.id == "capacitespe") {
-            capaciteSpeciale
+        if (btn.id == "capacitespe1") {
+            console.log("capacitespe1");
         }
 
        manette1[0].disabled = true
@@ -195,16 +198,28 @@ for (const btn of manette1){
        persof2.id = '';
     })
 }
+
+//perso Guerrier
 for (const btn of manette2){
     btn.addEventListener('click', () => {
-        if (btn.id == "attaquer"){
-            console.log("attaquer")
+        if (btn.id == "attaquer2"){
+
+            let degats = persoG.pointdattaque - persoG.pointdedéfense;
+            degats = Math.max(0, degats);
+            persoG.vie -= degats;
+
+            if (persoG2.vie <= 20) {
+                persoG2.capacitespeciale; }
+                
+            persoG2.vie;
+            console.log(persoG2.vie, "attaque2");
         }
-        if (btn.id == "defense") {
-            console.log("defense") 
+        if (btn.id == "defense2") {
+            (persoG2.pointdedéfense + persoG2.vie == persoG2.pointdedéfense);
+            console.log(persoG2.pointdedéfense),"def2";
         }
-        if (btn.id == "capacitespe") {
-            console.log("capspe")
+        if (btn.id == "capacitespe2") {
+            console.log("capspe2")
         }
         manette1[0].disabled = false
         manette1[1].disabled = false
@@ -219,28 +234,53 @@ for (const btn of manette2){
 
 
 
-// let persoG = {
-//     "vie" : "100%",
-//     "point d'attaque" : "25%",
-//     "point de défense" : "10%",
-//     "capacite speciale" : "30%",
+
+
+// // Fonction pour activer la capacité spéciale d'un joueur
+// function activerCapaciteSpeciale(joueur) {
+//     // Implémentez ici la logique de la capacité spéciale
+//     console.log("Capacité spéciale activée pour le joueur !");
 // }
-// let persoM = {
-//     "vie" : "100%",
-//     "point d'attaque" : "20%",
-//     "point de défense" : "15%",
-//     "capacite speciale" : "40%",
+
+// document.getElementById('attaquer').addEventListener('click', attaquer);
+// document.getElementById('defense').addEventListener('click', defendre);
+// document.getElementById('capacitespe').addEventListener('click', capaciteSpeciale);
+
+// const pointsDeVieInitiaux = { joueur1: 100, joueur2: 100,};
+
+// const pointsDeVieActuels = { joueur1: pointsDeVieInitiaux.joueur1, 
+//     joueur2: pointsDeVieInitiaux.joueur2 };
+
+
+// function mettreAJourPointsDeVie() {
+//     document.querySelector('.vie1').style.width = `${pointsDeVieActuels.joueur1}%`;
+//     document.querySelector('.vie2').style.width = `${pointsDeVieActuels.joueur2}%`;
+//     if (pointsDeVieInitiaux = O ) {
+//         "Partie fini "
+//     }
+//   // Ajoutez ici des conditions pour gérer la fin du jeu si les points de vie atteignent zéro
 // }
-// let persoP ={
-//     "vie" : "100%",
-//     "point d'attaque" : "10%",
-//     "point de défense" : "10%",
-//     "capacite speciale" : "60%",
-// }
-//  let persoA = {
-//     "vie" : "100%",
-//     "point d'attaque" : "15%",
-//     "point de défense" : "5%",
-//     "capacite speciale" : "40%",
-// }
+
+// function attaquer() {
+//     const degats = Math.floor(Math.random() * 20) + 10; 
+//     // Exemple de dégâts aléatoires entre 10 et 30
+//     // Appliquer les dégâts à l'adversaire
+//     pointsDeVieActuels.joueur2 -= degats;
+//     // Mettre à jour l'affichage des points de vie
+//     mettreAJourPointsDeVie();
+//   }
+
+// function defendre() {
+//     const reductionDegats = 0.5; // Exemple de réduction des dégâts de 50%
+//     // Réduire les dégâts subis lors de la prochaine attaque
+//     // Ajoutez ici la logique pour la défense
+//   }
+
+// function capaciteSpeciale() {
+//     const degatsCapacite = 30; // Exemple de dégâts de capacité spéciale
+//     // Appliquer les dégâts de capacité spéciale à l'adversaire
+//     pointsDeVieActuels.joueur2 -= degatsCapacite;
+//     // Mettre à jour l'affichage des points de vie
+//     mettreAJourPointsDeVie();
+//   }
 
