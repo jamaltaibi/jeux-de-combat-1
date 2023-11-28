c3cabfaedcaecf909b20de6038779faa4171c508
Your Name <jamaltaibi@MacBook-Pro-de-Jamal.local>
2023-11-28 09:59:15 +0100

    Jeux de Combat Tour par Tour

--- script/script.js
+++ script/script.js
@@ -14,7 +14,7 @@ function activerBoutonCommencer() {
     if (joueur1Choisi && joueur2Choisi) {
       newgame.removeAttribute('disabled');
     }
-  }
+}
 
 //Arene//
 const guerriers = document.querySelectorAll('.Guerrier')
@@ -30,7 +30,7 @@ let choixJ2 ;
 
 let perso = { 
     'persoG' : { "vie" : "100","pointdattaque" : "25","pointdedéfense" : "10","capacitespeciale" : "15",},
-    'persoM': { "vie" : "100","pointdattaque" : "20","pointdedéfense" : "10","capacitespeciale" : "20",},
+    'persoM' : { "vie" : "100","pointdattaque" : "20","pointdedéfense" : "10","capacitespeciale" : "20",},
     'persoP' : { "vie" : "100","pointdattaque" : "15","pointdedéfense" : "10","capacitespeciale" : "30",},
     'persoA' : { "vie" : "100","pointdattaque" : "20","pointdedéfense" : "5","capacitespeciale" : "30",},
 }
@@ -64,11 +64,9 @@ guerrier.addEventListener('click', () => {
         joueur2Choisi = true;
         activerBoutonCommencer();
 
-        choixJ2 = perso2['persoG']
-        
+        choixJ2 = perso2['persoG']  
     }  
 })}
-
 for (const mage of mages){
 mage.addEventListener('click', () => {
     if (mage.id == "1") {
@@ -83,7 +81,6 @@ mage.addEventListener('click', () => {
         choixJ1 = perso['persoM'] 
     }
     
-
     if (mage.id == "2") {
         const M = document.querySelector('.perso2')
         M.src = "../img/mage.png" 
@@ -97,7 +94,6 @@ mage.addEventListener('click', () => {
     }
     
 })}
-
 for (const pretre of pretres){
 pretre.addEventListener('click', () => {
     if (pretre.id == "1") {
@@ -125,7 +121,6 @@ pretre.addEventListener('click', () => {
     
   
 })}
-
 for (const archer of archers){
 archer.addEventListener('click', () => {
     if (archer.id == "1") {
@@ -152,7 +147,6 @@ archer.addEventListener('click', () => {
     }
 })
 }
-
 //Zone de combat//
 const manette1 = document.querySelectorAll('.manette button')
 const manette2 = document.querySelectorAll('.manette2 button')
@@ -161,32 +155,28 @@ const persof2 = document.querySelector('.persof2')
 const vie1 = document.querySelector('.vie1')
 const vie2 = document.querySelector('.vie2')
 
-function activerCapaciteSpeciale(joueur) {
-    
-}
-
 for (const btn of manette1){
     btn.addEventListener('click', () => {
         if (btn.id == "attaquer1"){
             let degats = choixJ1.pointdattaque - choixJ2.pointdedéfense;
             choixJ2.vie -= degats
-
-            if (choixJ2.vie <= 40) {
-                manette2[1].disabled = false
-            }
         }
-
         if (btn.id == "capacitespe1") {
             choixJ2.vie -= choixJ1.capacitespeciale
             manette1[1].disabled = true
+        }
+
+        if (choixJ2.vie <= 40) {
+            manette2[1].disabled = false
         }
 
         if (choixJ2.vie <= 0) {
             vie2.style.width = '0%'
             zonedecombat.style.display = "none";
             gameover.style.display = "flex"
+            document.querySelector('.winnerIs').innerHTML = "Victoire du Joueur 1"
         }
-      
+      console.log(choixJ2.vie);
        manette1[0].disabled = true
        manette2[0].disabled = false
        
@@ -202,23 +192,23 @@ for (const btn of manette2){
         if (btn.id == "attaquer2"){
             let degats = choixJ2.pointdattaque - choixJ1.pointdedéfense;
             choixJ1.vie -= degats
-
-            if (choixJ2.vie <= 40) {
-                manette1[1].disabled = false
-            } 
         }
-
         if (btn.id == "capacitespe2") {
             choixJ1.vie -= choixJ2.capacitespeciale
             manette2[1].disabled = true
+        }
+
+        if (choixJ1.vie <= 40) {
+            manette1[1].disabled = false
         }
 
         if(choixJ1.vie <= 0){
             vie1.style.width = '0%'
             zonedecombat.style.display = "none";
             gameover.style.display = "flex"
+            document.querySelector('.winnerIs').innerHTML = "Victoire du Joueur 2"
         }
-
+        console.log(choixJ1.vie);
         manette1[0].disabled = false
         manette2[0].disabled = true
         
